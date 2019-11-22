ba9723ec915aba5a3282ec477bd625df6d4ba208
zbenedek <benedek.zoltan@aut.bme.hu>
2019-11-22 22:04:48 +0100

    REST előadás példája .NET Core 3.0-ra portolva

---
+++ Rest-WebApi/TodoApi/TodoApi/Data/TodoApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoApiContext : DbContext
+    {
+        public TodoApiContext (DbContextOptions<TodoApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoApi.Models.TodoItem> TodoItem { get; set; }
+    }
+}
